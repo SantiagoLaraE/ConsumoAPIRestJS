7ed49cac4c31b72c63bf90dce36d3c36be2e29ec
Santiago Lara <oscarescobar773@gmail.com>
2022-04-27 22:33:31 -0500

    Se genera HTML dinámico y se arregla la función POST para agregar los gatos favoritos, también se arreglan algunos estilos

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=202375a2-f56a-4091-8c61-edc226939576';
 const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?api_key=202375a2-f56a-4091-8c61-edc226939576';
-const button = document.getElementById('randomCat');
 const spanError = document.getElementById('error');
+
 /*
 // PROMESAS
 
@@ -24,45 +24,87 @@ button.onclick = () => {renderCat()};
 async function loadRandomCats(){
     const response = await fetch(API_URL_RANDOM);
     const cats = await response.json();
-    const imgHTML1 = document.getElementById('img1');
-    const imgHTML2 = document.getElementById('img2');
-    const imgHTML3 = document.getElementById('img3');
 
-    console.log(cats)
     if(response.status !== 200){
         spanError.innerHTML = 'Hubo un error: ' + response.status;
     }else{
-        imgHTML1.src = cats[0].url;
-        imgHTML2.src = cats[1].url;
-        imgHTML3.src = cats[2].url; 
+        const section = document.getElementById('randomCats');
+
+        const sectionContainer = document.createElement('section');
+        sectionContainer.classList.add('section__container');
+        
+        cats.forEach(cat => {
+            const article = document.createElement('article');
+
+            const img = document.createElement('img');
+            img.src = cat.url;
+
+            const btn = document.createElement('button');
+            const btnText = document.createTextNode('Agregar a favoritos');
+            btn.classList.add('btn-primary')
+            btn.appendChild(btnText);
+
+            btn.onclick = () => saveFavoriteCat(cat.id);
+
+            article.appendChild(img);
+            article.appendChild(btn);
+
+            sectionContainer.appendChild(article);
+
+        })
+
+        section.appendChild(sectionContainer);
     }
 }
 
-loadRandomCats()
-button.onclick = () => loadRandomCats();
+loadRandomCats();
 
 
-async function getFavoritesCats(){
+async function loadFavoritesCats(){
     const response = await fetch(API_URL_FAVORITES);
     const data = await response.json();
 
     if(response.status !== 200){
         spanError.innerHTML = `Hubo un error: ${data.status}.`;
     }else{
-        console.log(data)
+        const section = document.getElementById('favoritesCats');
+
+        const sectionContainer = document.createElement('div');
+        sectionContainer.classList.add('section__container');
+
+        data.forEach(cat => {
+            
+            const article = document.createElement('article');
+
+            const img = document.createElement('img');
+
+            const btn = document.createElement('button');
+            btn.classList.add('btn-primary')
+
+            const btnText = document.createTextNode('Sacar de favoritos');
+            btn.appendChild(btnText)
+            
+            img.src = cat.image.url;
+
+            article.appendChild(img);
+            article.appendChild(btn);
+
+            sectionContainer.appendChild(article);
+        });
+        section.appendChild(sectionContainer);
     }
 
 }
-getFavoritesCats();
+loadFavoritesCats();
 
-async function saveFavoriteCat(){
+async function saveFavoriteCat(id){
     const res = await fetch(API_URL_FAVORITES, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            'image_id': '7ce'
+            'image_id': id,
         })
     })
     const data = res.json();
